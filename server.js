a8d85e8a3835b357f69a4f17d0f470d84d0bbaf1
Nghia Nguyen <nghia.nguyen@vietan-software.com>
2020-04-16 00:04:06 +0700

    DONE SERVER USE SOCKET IO SHOW USER ONLINE,AND WEB CLIENT SHOW AND CALL

--- server.js
+++ server.js
@@ -1,119 +1,131 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var open = require('open');
-var serverPort = (4443);
-var http = require('http');
+var open = require("open");
+var serverPort = 4443;
+var http = require("http");
 var server = http.createServer(app);
-var io = require('socket.io')(server);
-// var io = require('socket.io')(3000);
+var io = require("socket.io")(server);
 
 var sockets = {};
 var users = {};
-function sendTo(connection, message) {
-   connection.send(message);
-}
-app.use(express.static(__dirname + '/')); //add public static file ( cho client )
 
-app.get('/', function(req, res){
-  console.log('get /');
-  res.sendFile(__dirname + '/index.html');
+function sendTo(connection, message) {
+  connection.send(message);
+}
+app.use(express.static(__dirname + "/")); //add public static file ( cho client )
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
+const arrayPeerId = [];
 
-io.on('connection', function(socket){
+io.on("connection", function(socket) {
   console.log("user connected");
 
-  socket.on('disconnect', function () {
-    console.log("user disconnected");
-    if(socket.name){
-      socket.broadcast.to("chatroom").emit('roommessage',{ type: "disconnect", username: socket.name})
-      delete sockets[socket.name];
-      delete users[socket.name];
-    }
+  socket.on('NEW_PEER_ID',peerId => {
+      socket.peerId = peerId;
+      arrayPeerId.push(peerId);
+      console.log("New client connect : ", peerId);
+      io.emit('NEW_CLIENT_CONNECT',peerId)
+  });
+  //gui danh sach online hien tai
+  socket.emit('ONLINE_PEER_ARRAY',arrayPeerId);
 
-  })
+  socket.on("disconnect", function() {
+    console.log("user disconnected : " ,socket.peerId);
+    const indexPeerId = arrayPeerId.indexOf(socket.peerId);
+    arrayPeerId.splice(indexPeerId,1)
+    //phat su kien cho toan bo user
+    io.emit('PEER_ID_DISCONNECT', socket.peerId)
+    delete socket.peerId
 
-  socket.on('message', function(message){
+   //  if (socket.name) {
+   //    socket.broadcast
+   //      .to("chatroom")
+   //      .emit("roommessage", { type: "disconnect", username: socket.name });
+   //    delete sockets[socket.name];
+   //    delete users[socket.name];
+   //  }
+  });
 
-    var data = message;
 
-    switch (data.type) {
+  //   socket.on('message', function(message){
+  //     var data = message;
+  //     switch (data.type) {
+  //     case "login":
+  //       console.log("User logged", data.name);
+  //       //if anyone is logged in with this username then refuse
+  //       if(sockets[data.name]) {
+  //          sendTo(socket, {
+  //             type: "login",
+  //             success: false
+  //          });
+  //       } else {
+  //          //save user connection on the server
+  //          var templist = users;
+  //          sockets[data.name] = socket;
+  //          socket.name = data.name;
+  //          sendTo(socket, {
+  //             type: "login",
+  //             success: true,
+  //             username: data.name,
+  //             userlist: templist
+  //          });
+  //          socket.broadcast.to("chatroom").emit('roommessage',{ type: "login", username: data.name})
+  //          socket.join("chatroom");
+  //          users[data.name] = socket.id
+  //       }
 
-    case "login":
-      console.log("User logged", data.name);
+  //       break;
+  //       case "call_user":
+  //       // chek if user exist
+  //         if(sockets[data.name]){
+  //           console.log("user called");
+  //           console.log(data.name);
+  //           console.log(data.callername);
+  //         sendTo(sockets[data.name], {
+  //            type: "answer",
+  //            callername: data.callername
+  //         });
+  //       }else{
+  //         sendTo(socket, {
+  //            type: "call_response",
+  //            response: "offline"
+  //         });
+  //       }
+  //       break;
+  //       case "call_accepted":
+  //       sendTo(sockets[data.callername], {
+  //          type: "call_response",
+  //          response: "accepted",
+  //          responsefrom : data.from
 
-      //if anyone is logged in with this username then refuse
-      if(sockets[data.name]) {
-         sendTo(socket, {
-            type: "login",
-            success: false
-         });
-      } else {
-         //save user connection on the server
-         var templist = users;
-         sockets[data.name] = socket;
-         socket.name = data.name;
-         sendTo(socket, {
-            type: "login",
-            success: true,
-            username: data.name,
-            userlist: templist
-         });
-         socket.broadcast.to("chatroom").emit('roommessage',{ type: "login", username: data.name})
-         socket.join("chatroom");
-         users[data.name] = socket.id
-      }
+  //       });
+  //       break;
+  //       case "call_rejected":
+  //       sendTo(sockets[data.callername], {
+  //          type: "call_response",
+  //          response: "rejected",
+  //          responsefrom : data.from
+  //       });
+  //       break;
+  //       case "call_busy":
+  //       sendTo(sockets[data.callername], {
+  //          type: "call_response",
+  //          response: "busy",
+  //          responsefrom : data.from
+  //       });
+  //       default:
+  //       sendTo(socket, {
+  //          type: "error",
+  //          message: "Command not found: " + data.type
+  //       });
+  //       break;
+  // }
+  //   })
+});
 
-      break;
-      case "call_user":
-      // chek if user exist
-        if(sockets[data.name]){
-          console.log("user called");
-          console.log(data.name);
-          console.log(data.callername);
-        sendTo(sockets[data.name], {
-           type: "answer",
-           callername: data.callername
-        });
-      }else{
-        sendTo(socket, {
-           type: "call_response",
-           response: "offline"
-        });
-      }
-      break;
-      case "call_accepted":
-      sendTo(sockets[data.callername], {
-         type: "call_response",
-         response: "accepted",
-         responsefrom : data.from
-
-      });
-      break;
-      case "call_rejected":
-      sendTo(sockets[data.callername], {
-         type: "call_response",
-         response: "rejected",
-         responsefrom : data.from
-      });
-      break;
-      case "call_busy":
-      sendTo(sockets[data.callername], {
-         type: "call_response",
-         response: "busy",
-         responsefrom : data.from
-      });
-      default:
-      sendTo(socket, {
-         type: "error",
-         message: "Command not found: " + data.type
-      });
-      break;
-}
-
-  })
-})
-
-server.listen( process.env.PORT ||  serverPort, function(){
-   console.log('server up and running at %s port', serverPort);
- });
+server.listen(process.env.PORT || serverPort, function() {
+  console.log("server up and running at %s port", serverPort);
+});
--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const uid = require('uid');
 const $ = require('jquery');
 const openStream = require('./openStream');
 const playVideo = require('./playVideo');
+import io from 'socket.io-client';
+const socket = io();
+socket.on('connect', function(data) {
+    console.log('socket client connect success');
+});
 // const getIceObject = require('./getIceObject');
-
 // getIceObject(iceConfig => {
     const connectionObj = {
         host: '9000-a3f10ba9-25e4-447e-af24-103f293291ba.ws-us02.gitpod.io',
@@ -14,9 +18,11 @@ const playVideo = require('./playVideo');
         // key: 'myapp',
         // config: iceConfig
     };
+    const peerId = getPeer();
+    //gui PeerId len server socket khi connect
+    socket.emit('NEW_PEER_ID', peerId);
 
-    const peer = new Peer(getPeer(), connectionObj);
-
+    const peer = new Peer(peerId, connectionObj);
     $('#btnCall').click(() => {
         const friendId = $('#txtFriendId').val();
         openStream(stream => {
@@ -33,15 +39,34 @@ const playVideo = require('./playVideo');
             call.on('stream', remoteStream => playVideo(remoteStream, 'friendStream'));
         });
     });
-
-// });
-
 function getPeer() {
     const id = uid(10);
     $('#peer-id').append(id);
     return id;
 }
 
+$(`#ulPeerId`).on('click','li', function(){
+    const friendId = $(this).text();
+        openStream(stream => {
+            playVideo(stream, 'localStream');
+            const call = peer.call(friendId, stream);
+            call.on('stream', remoteStream => playVideo(remoteStream, 'friendStream'))
+        });
+})
+/**
+ * socket
+ *
+ */
+socket.on('ONLINE_PEER_ARRAY',arrayPeerId => {
+    arrayPeerId.forEach(peerId => {
+        $('#ulPeerId').append(`<li id='${peerId}'>${peerId}</li>`)
 
-
-
+    });
+})
+socket.on('NEW_CLIENT_CONNECT', peerId => {
+    console.log('New User Client : ' , peerId)
+    $('#ulPeerId').append(`<li id='${peerId}'>${peerId}</li>`)
+})
+socket.on('PEER_ID_DISCONNECT',peerId => {
+        $(`#${peerId}`).remove()
+})
